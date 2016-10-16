1b9b7e4714cb083b80cab5223cdd3ad3c4c6f1d9
SUSUMU Seino <seino@susu.mu>
2016-10-17 00:04:50 +0900

    KCC-27 shopパーツの切り分け #close

---
+++ themes/kitchencar-gourmet-championship/template-parts/content-shop.php
@@ -0,0 +1,25 @@
+<div class="frontShop">
+		<div class="frontShop__inner">
+				<div class="frontShop__headline"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/title_shop.png" alt=""></div>
+				<div class="frontShop__inner col-3">
+						<div class="col-3__item">
+								<a href="#">
+										<div class="col-3__img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/thumbnail.jpg" alt=""></div>
+										<div class="col-3__title frontShop">お店の名前</div>
+								</a>
+						</div>
+						<div class="col-3__item">
+								<a href="#">
+										<div class="col-3__img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/thumbnail.jpg" alt=""></div>
+										<div class="col-3__title frontShop">コロコロチキンラーメン</div>
+								</a>
+						</div>
+						<div class="col-3__item">
+								<a href="#">
+										<div class="col-3__img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/thumbnail.jpg" alt=""></div>
+										<div class="col-3__title frontShop">アジアンランチ</div>
+								</a>
+						</div>
+				</div><a href="#" class="btn">もっと見る</a>
+		</div>
+</div>
