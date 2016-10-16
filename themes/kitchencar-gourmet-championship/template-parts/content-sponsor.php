26ab51515026b866da834a0f620587c64e540d78
SUSUMU Seino <seino@susu.mu>
2016-10-17 00:05:51 +0900

    KCC-33 sponsorパーツの切り分け #close

---
+++ themes/kitchencar-gourmet-championship/template-parts/content-sponsor.php
@@ -0,0 +1,16 @@
+<div class="frontSponsor">
+		<div class="frontSponsor__inner">
+				<div class="frontSponsor__headline"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/title_sponsor.png" alt=""></div>
+				<ul class="frontSponsor__list col-3">
+						<li class="frontSponsor__item col-3__item">
+								<a target="_blank" href="http://www.nack5.co.jp/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/logo-nack5.jpg" alt=""></a>
+						</li>
+						<li class="frontSponsor__item col-3__item">
+								<a target="_blank" href="http://www.reds-ss.com/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/logo-reds.png" width="60px" alt=""></a>
+						</li>
+						<li class="frontSponsor__item col-3__item">
+								<a target="_blank" href="https://www.w-tokyodo.com/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/logo-tokyodo.jpg" alt=""></a>
+						</li>
+				</ul>
+		</div>
+</div>
