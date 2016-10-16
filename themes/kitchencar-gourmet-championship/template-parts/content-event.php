f14a4616d59d38624b8e5f43c30f8f44df14384a
SUSUMU Seino <seino@susu.mu>
2016-10-17 00:04:41 +0900

    KCC-28 eventパーツの切り分け #close

---
+++ themes/kitchencar-gourmet-championship/template-parts/content-event.php
@@ -0,0 +1,14 @@
+<div class="frontEvent">
+				<div class="frontEvent__inner">
+						<div class="frontEvent__headline"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/title_event.png" alt=""></div>
+						<div class="frontEvent__content">
+							<p>オーナーの個性を表した素敵なキッチンカーが、自慢のグルメを持って日本各地から大集合！
+							</p>
+							<p>
+							レストランで腕を磨いた経験や、こだわり抜いた開発した独自メニューなど自慢の一品を提供している「キッチンカー」のNo.1を決定します！アジア最大級のサッカースタジアム「埼玉スタジアム2002」で、普段は味わえない味をお楽しみください。</p>
+						</div>
+						<div class="frontEvent__map"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/map.png" alt="">
+								<p>埼玉高速鉄道「浦和美園駅」から徒歩15分</p><small>※駐車台数に限りがありますので、公共交通機関にてご来場ください。なお、満車の際はご容赦ください。</small>
+						</div>
+				</div>
+		</div>
