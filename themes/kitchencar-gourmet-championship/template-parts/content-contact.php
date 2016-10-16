dd9e6be5a8c292aaa57a4d33e5aff2cfb88e9d0e
SUSUMU Seino <seino@susu.mu>
2016-10-17 00:05:01 +0900

    KCC-26 contactパーツの切り分け #close

---
+++ themes/kitchencar-gourmet-championship/template-parts/content-contact.php
@@ -0,0 +1,9 @@
+		<div class="frontContact">
+				<div class="frontContact__inner">
+						<div class="frontContact__headline"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/title_contact.png" alt=""></div>
+						<ul class="frontContact__list"></ul>
+						<li class="frontContact__item">株式会社ワークストア・トウキョウドゥ</li>
+						<li class="frontContact__item">電　話：03-3737-3000</li>
+						<li class="frontContact__item"><a href="https://www.w-tokyodo.com/contact" target="_blank"></a>お問い合わせ：https://www.w-tokyodo.com/contact</li>
+				</div>
+		</div>
