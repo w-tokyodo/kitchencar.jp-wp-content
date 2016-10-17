ed46c986bc58d417515f46bf48f3713a0d76d433
SUSUMU Seino <seino@susu.mu>
2016-10-17 15:47:01 +0900

    スポンサーのcssを修正

--- themes/kitchencar-gourmet-championship/template-parts/content-sponsor.php
+++ themes/kitchencar-gourmet-championship/template-parts/content-sponsor.php
@@ -1,15 +1,18 @@
 <div class="frontSponsor">
 		<div class="frontSponsor__inner">
 				<div class="frontSponsor__headline"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/title_sponsor.png" alt=""></div>
-				<ul class="frontSponsor__list col-3">
-						<li class="frontSponsor__item col-3__item">
-								<a target="_blank" href="http://www.nack5.co.jp/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/logo-nack5.jpg" alt=""></a>
+				<ul class="frontSponsor__list">
+						<li class="frontSponsor__item">
+								<a target="_blank" href="http://www.nack5.co.jp/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/sponsor-nack5.png" alt=""></a>
 						</li>
-						<li class="frontSponsor__item col-3__item">
-								<a target="_blank" href="http://www.reds-ss.com/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/logo-reds.png" width="60px" alt=""></a>
+						<li class="frontSponsor__item">
+								<a target="_blank" href="http://www.reds-ss.com/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/sponsor-reds.png" width="60px" alt=""></a>
 						</li>
-						<li class="frontSponsor__item col-3__item">
-								<a target="_blank" href="https://www.w-tokyodo.com/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/logo-tokyodo.jpg" alt=""></a>
+						<li class="frontSponsor__item">
+								<a target="_blank" href="https://www.w-tokyodo.com/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/sponsor-tokyodo.png" alt=""></a>
+						</li>
+						<li class="frontSponsor__item">
+								<a href="https://www.w-tokyodo.com/contact" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/sponsor-contact.png" alt=""></a>
 						</li>
 				</ul>
 		</div>
