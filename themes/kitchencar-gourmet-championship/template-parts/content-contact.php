2830e05d7bf8e50e1eeefa0bc3ffcf8eb45ad27e
SUSUMU Seino <seino@susu.mu>
2016-10-17 15:45:26 +0900

    お問合せの修正

--- themes/kitchencar-gourmet-championship/template-parts/content-contact.php
+++ themes/kitchencar-gourmet-championship/template-parts/content-contact.php
@@ -1,9 +1,20 @@
 		<div class="frontContact">
 				<div class="frontContact__inner">
 						<div class="frontContact__headline"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/title_contact.png" alt=""></div>
-						<ul class="frontContact__list"></ul>
-						<li class="frontContact__item">株式会社ワークストア・トウキョウドゥ</li>
-						<li class="frontContact__item">電　話：03-3737-3000</li>
-						<li class="frontContact__item"><a href="https://www.w-tokyodo.com/contact" target="_blank"></a>お問い合わせ：https://www.w-tokyodo.com/contact</li>
+						<dl class="frontContact__item">
+							<dt class="frontContact__title">開催日時</dt>
+							<dd class="frontContact__description">2016年12月4日 10:00~16:00</dd>
+							<dt class="frontContact__title">開催場所</dt>
+							<dd class="frontContact__description">埼玉スタジアム2002 公園 北広場</dd>
+							<dt class="frontContact__title">入場料</dt>
+							<dd class="frontContact__description">無料</dd>
+							<dt class="frontContact__title">主　催</dt>
+							<dd class="frontContact__description">株式会社ワークストア・トウキョウドゥ</dd>
+							<dt class="frontContact__title">電　話</dt>
+							<dd class="frontContact__description">03-3737-3000</dd>
+							<dt class="frontContact__title">お問合せ</dt>
+							<dd class="frontContact__description"><a href="https://www.w-tokyodo.com/contact" target="_blank">https://www.w-tokyodo.com/contact/</a></dd>
+						</dl>
+						
 				</div>
 		</div>
