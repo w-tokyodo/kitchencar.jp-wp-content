a22bc5e19781ca3150a16df26cf0c90bc2a2f342
SUSUMU Seino <seino@susu.mu>
2016-10-17 16:32:38 +0900

    KCC-35 スポンサー部分の非表示 #close

--- themes/kitchencar-gourmet-championship/index.php
+++ themes/kitchencar-gourmet-championship/index.php
@@ -22,7 +22,7 @@ get_header(); ?>
 		<?php get_template_part("template-parts/content","event"); ?>
 		<!-- <?php get_template_part("template-parts/content","shop"); ?> -->
 		<?php get_template_part("template-parts/content","contact"); ?>
-		<?php get_template_part("template-parts/content","sponsor"); ?>
+		<!-- <?php get_template_part("template-parts/content","sponsor"); ?> -->
 <?php
 //get_sidebar();
 get_footer();
