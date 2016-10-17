8b6714e6b27c74746422d4d848ffa93e0ce7324b
SUSUMU Seino <seino@susu.mu>
2016-10-17 13:11:25 +0900

    KCC-13 page.phpの実装 #close

--- themes/kitchencar-gourmet-championship/page.php
+++ themes/kitchencar-gourmet-championship/page.php
@@ -9,30 +9,37 @@
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
- * @package KitchinCar_Gourmet_Championship
+ * @package KitchenCar_Gourmet_Championship
  */
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div class="content-header">
+	<div class="content-header__image">
+		<a href="<?php echo esc_url(home_url( '/' )); ?>">
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/single-header.png" >
+		</a>
+	</div>
+</div>
 
-			<?php
-			while ( have_posts() ) : the_post();
+<div class="content-area">
+	<?php while(have_posts()): the_post(); ?>
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<header class="entry-header">
+				<h1 class="entry-title">
+					<?php the_title(); ?>
+				</h1>
+			</header>
+			<div class="entry-content">
+				<?php the_content(); ?>
+			</div>
+		</article>
+	<?php endwhile; ?>
+</div>
 
-				get_template_part( 'template-parts/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-			endwhile; // End of the loop.
-			?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		<?php get_template_part("template-parts/content","contact"); ?>
+		<?php get_template_part("template-parts/content","sponsor"); ?>
 
 <?php
-get_sidebar();
 get_footer();
+
