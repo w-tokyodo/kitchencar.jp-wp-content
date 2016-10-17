9316259b0b54a785fcadfaeffad1e41f62032988
SUSUMU Seino <seino@susu.mu>
2016-10-17 20:38:29 +0900

    specialを非表示

--- themes/kitchencar-gourmet-championship/footer.php
+++ themes/kitchencar-gourmet-championship/footer.php
@@ -10,22 +10,7 @@
  */
 
 ?>
-		<div class="frontSpecial">
-				<div class="frontSpecial__inner">
-					<div class="frontSpecial__col-2--left">
-						<a href="http://www.w-tokyodo.com/neostall/" target="_blank">
-								<div class="frontSpecial__image"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/logo-neostall.png" alt=""></div>
-								<div class="frontSpecial__text">Powered by Tokyo do.</div>
-						</a>
-					</div>
-					<div class="frontSpecial__col-2--right">
-						<a href="http://foodear.jp" target="_blank">
-								<div class="frontSpecial__image"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/logo-foodear.png" width="100px" alt=""></div>
-								<div class="frontSpecial__text">Design by foodear.</div>
-						</a>
-					</div>
-				</div>
-		</div>
+		<!-- <?php get_template_part('template-parts/content','special' ); ?> -->
 		<div class="frontCopyright">
 				<div class="frontCopyright__inner">
 						<p>Copyright (C) 2016 キッチンカースタジアムグルメ選手権. All Rights Reserved.</p>
---
+++ themes/kitchencar-gourmet-championship/template-parts/content-special.php
@@ -0,0 +1,16 @@
+<div class="frontSpecial">
+				<div class="frontSpecial__inner">
+					<div class="frontSpecial__col-2--left">
+						<a href="http://www.w-tokyodo.com/neostall/" target="_blank">
+								<div class="frontSpecial__image"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/logo-neostall.png" alt=""></div>
+								<div class="frontSpecial__text">Powered by Tokyo do.</div>
+						</a>
+					</div>
+					<div class="frontSpecial__col-2--right">
+						<a href="http://foodear.jp" target="_blank">
+								<div class="frontSpecial__image"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/logo-foodear.png" width="100px" alt=""></div>
+								<div class="frontSpecial__text">Design by foodear.</div>
+						</a>
+					</div>
+				</div>
+		</div>
