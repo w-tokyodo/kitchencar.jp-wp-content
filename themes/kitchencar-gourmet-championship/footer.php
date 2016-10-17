0a15c90c303bd523ba45f25d57296f368bff0202
SUSUMU Seino <seino@susu.mu>
2016-10-17 17:22:55 +0900

    KCC-36 Foodear の扱い方について

--- themes/kitchencar-gourmet-championship/footer.php
+++ themes/kitchencar-gourmet-championship/footer.php
@@ -6,21 +6,32 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package KitchinCar_Gourmet_Championship
+ * @package KitchenCar_Gourmet_Championship
  */
 
 ?>
-
 		<div class="frontSpecial">
-			<div class="frontSpecial__inner"><a href="http://foodear.jp" target="_blank">
-					<div class="frontSpecial__image"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/logo-foodear.png" alt=""></div>
-					<div class="frontSpecial__text">foodearはキッチンカーグルメ選手権とキッチンカーを愛するすべての人を応援します!!</div></a></div>
+				<div class="frontSpecial__inner">
+					<div class="frontSpecial__col-2--left">
+						<a href="http://www.w-tokyodo.com/neostall/" target="_blank">
+								<div class="frontSpecial__image"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/logo-neostall.png" alt=""></div>
+								<div class="frontSpecial__text">Powered by Tokyo do.</div>
+						</a>
+					</div>
+					<div class="frontSpecial__col-2--right">
+						<a href="http://foodear.jp" target="_blank">
+								<div class="frontSpecial__image"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/logo-foodear.png" width="100px" alt=""></div>
+								<div class="frontSpecial__text">Design by foodear.</div>
+						</a>
+					</div>
+				</div>
 		</div>
 		<div class="frontCopyright">
-			<div class="frontCopyright__inner">
-				<p>Copyright (C) 2016 キッチンカースタジアムグルメ選手権. All Rights Reserved.</p>
-			</div>
+				<div class="frontCopyright__inner">
+						<p>Copyright (C) 2016 キッチンカースタジアムグルメ選手権. All Rights Reserved.</p>
+				</div>
 		</div>
 		<?php wp_footer(); ?>
-	</body>
-</html>
+		</body>
+
+		</html>
