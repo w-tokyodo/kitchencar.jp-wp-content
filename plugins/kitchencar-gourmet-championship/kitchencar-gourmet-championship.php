be7d93b15a32a2fb45f1cc6d1ffc052776c11570
mimoto.t <mimoto@w-tokyodo.com>
2016-10-19 13:00:21 +0900

    create main plugin

---
+++ plugins/kitchencar-gourmet-championship/kitchencar-gourmet-championship.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name:     Kitchencar Gourmet Championship
+ * Text Domain:     kitchencar-gourmet-championship
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Kitchencar_Gourmet_Championship
+ */
+
+define( 'KGC_PATH', __DIR__ );
+define( 'KGC_URI',  plugins_url( '', __FILE__ ) );
+
+/**
+ * Google Analytics
+ */
+function kgc_add_ga_codes() {
+	$uids = [ /** mimoto */ 'UA-26079619-1' ];
+	if ( $uids && ! is_user_logged_in() ) {
+		$code = "\tga('create', '%s', 'auto');";
+		$eocode = "\tga('send', 'pageview');";
+		wp_enqueue_script( 'ga', trailingslashit( KGC_URI ) . 'js/ga.js', [], '', true );
+		foreach ( $uids as $uid ) {
+			wp_add_inline_script( 'ga', sprintf( $code, $uid ) );
+		}
+		wp_add_inline_script( 'ga', $eocode );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'kgc_add_ga_codes' );
