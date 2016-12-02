67af3cc9324fd3180a0f7121bfc4a8cbf9b534dd
mimosafa <mimosafa@gmail.com>
2016-12-02 12:28:46 +0900

    hotfix

--- themes/kitchencar-gourmet-championship/single-kgc_shop.php
+++ themes/kitchencar-gourmet-championship/single-kgc_shop.php
@@ -37,11 +37,13 @@ get_header(); ?>
 					<?php echo SCF::get('shop-content'); ?>
 				</div>
 				<div class="entry-fields__menu">
-				<?php $p_player_group = SCF::get( 'shop-menu-item' ); foreach ( $p_player_group as $field_name => $field_value ) { 	?>
+				<?php if ( $p_player_group = SCF::get( 'shop-menu-item' ) ) { 	?>
+				<?php foreach ( $p_player_group as $field_name => $field_value ) { 	?>
 					<dl>
 						<td><?php echo esc_html( $field_value['shop-menu-item'] ); ?></td>
 						<td><?php echo esc_html( $field_value['shop-menu-price'] ); ?>円</td>
 					</dl>
+				<?php } ?>
 				<?php } ?>
 				</div>
 			</div>
