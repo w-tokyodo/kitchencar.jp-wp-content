c241d02ff915fdcac146a5856b053a15d49e6699
SUSUMU Seino <seino@susu.mu>
2016-12-02 13:57:19 +0900

    出店情報の個別投稿ページのマークアップを変更

--- themes/kitchencar-gourmet-championship/single-kgc_shop.php
+++ themes/kitchencar-gourmet-championship/single-kgc_shop.php
@@ -19,38 +19,34 @@ get_header(); ?>
 
 <div class="content-area">
 	<?php while(have_posts()): the_post(); ?>
-		<article id="post-<?php the_ID(); ?> kgc_shop" <?php post_class(); ?>>
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 			<header class="entry-header">
-				<div class="entry-fields__thumbnail">
-					<?php the_post_thumbnail('kgc_thumbnail_post'); ?>
-				</div>
-				<h1 class="entry-title">
-					<?php the_title(); ?>
-				</h1>
-				<div class="entry-fields__copy">
-					<?php echo SCF::get('shop-copy'); ?>
-				</div>
-				<div class="entry-fields__thumbnail">
-					<?php the_post_thumbnail('kgc_thumbnail_post'); ?>
-					<?php $image = get_post_meta($post->ID, 'shop-image', true); echo wp_get_attachment_image($image, 'kgc_thumbnail_car');?>
-				</div>
 			</header>
-			<div class="entry-fields">
-				<div class="entry-fields__content">
-					<?php echo SCF::get('shop-content'); ?>
+			<div class="kgc_shop">
+				<div class="kgc_shop__column kgc_shop__food">
+					<?php the_post_thumbnail('kgc-food'); ?>
 				</div>
-				<div class="entry-fields__menu">
-					<dl>
-					<?php if(post_custom('shop-menu-item')): ?>
-						<dt>自慢の一品</dt>
-						<dd><?php echo SCF::get('shop-menu-item'); ?></dd>
-					<?php endif; ?>
-					<?php if(post_custom('shop-menu-price')): ?>
-						<dt>価格</dt>
-						<dd><?php echo SCF::get('shop-menu-price'); ?>円</dd>
-					<?php endif; ?>
-					</dl>
+				<div class="kgc_shop__column kgc_shop__info">
+					<h1 class="kgc_shop__title">
+						<?php the_title(); ?>
+					</h1>
+					<div class="kgc_shop__copy">
+						<?php echo SCF::get('shop-copy'); ?>
+					</div>
+					<div class="kgc_shop__menu">
+						<dl>
+						<?php if(post_custom('shop-menu-item')): ?>
+							<dt>自慢の一品</dt>
+							<dd><?php echo SCF::get('shop-menu-item'); ?></dd>
+						<?php endif; ?>
+						<?php if(post_custom('shop-menu-price')): ?>
+							<dt>価格</dt>
+							<dd><?php echo SCF::get('shop-menu-price'); ?>円</dd>
+						<?php endif; ?>
+						</dl>
+					</div>
 				</div>
+			</div>
 			</div>
 		</article>
 	<?php endwhile; ?>
