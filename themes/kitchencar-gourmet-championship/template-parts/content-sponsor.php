023bda945a5a64d3f3209f293ada5eea5a64c73c
mimosafa <mimosafa@gmail.com>
2016-12-02 12:21:02 +0900

    remove wstd from 協力

--- themes/kitchencar-gourmet-championship/template-parts/content-sponsor.php
+++ themes/kitchencar-gourmet-championship/template-parts/content-sponsor.php
@@ -4,9 +4,6 @@
 				<div class="list-wrapper">
 					<ul class="frontSponsor__list">
 							<li class="frontSponsor__item">
-									<a target="_blank" href="https://www.w-tokyodo.com/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/sponsor-tokyodo.png" alt=""></a>
-							</li>
-							<li class="frontSponsor__item">
 									<a target="_blank" href="http://foodear.jp/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/sponsor-foodear.png" alt=""></a>
 							</li>
 							<li class="frontSponsor__item">
