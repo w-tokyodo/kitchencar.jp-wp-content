dda9b9ef83d96be9fe29fec1e5586fd5d020c489
Susumu <seino@susu.mu>
2017-09-04 13:48:27 +0900

    KCC-70 開催情報の修正

--- themes/kitchencar-gourmet-championship/template-parts/content-event.php
+++ themes/kitchencar-gourmet-championship/template-parts/content-event.php
@@ -3,24 +3,23 @@
 						<div class="frontEvent__headline"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/title_event.png" alt=""></div>
 						<dl class="frontEvent__item">
 							<dt class="frontEvent__title">開催日時</dt>
-							<dd class="frontEvent__description">2016年12月4日 10:00~16:00 <small>（投票は15時まで） 雨天決行・荒天中止</small></dd>
+							<dd class="frontEvent__description">2017年10月28日 10:00~16:00 <small>（投票は15時まで） 雨天決行・荒天中止</small></dd>
 							<dt class="frontEvent__title">開催場所</dt>
-							<dd class="frontEvent__description">埼玉スタジアム2002 公園 南広場</dd>
+							<dd class="frontEvent__description">埼玉スタジアム2002 公園 北広場</dd>
 							<dt class="frontEvent__title">入場料</dt>
 							<dd class="frontEvent__description">無料</dd>
 							<dt class="frontEvent__title">主　催</dt>
 							<dd class="frontEvent__description">公益財団法人埼玉県公園緑地協会 埼玉スタジアム2002公園管理事務所</dd>
 							<dd class="frontEvent__description">株式会社ワークストア・トウキョウドゥ</dd>
 							<dt class="frontEvent__title">協　力</dt>
-							<dd class="frontEvent__description">浦和レッドダイヤモンズ</dd>
-							<dd class="frontEvent__description">浦和レッズ後援会</dd>
 							<dd class="frontEvent__description">ＴＳＰ太陽株式会社</dd>
+							<dd class="frontEvent__description">株式会社スポニチプライム</dd>
 						</dl>
 						<div class="frontEvent__content">
 							<p>オーナーの個性を表した素敵なキッチンカーが、自慢のグルメを持って日本各地から大集合！
 							</p>
 							<p>
-							レストランで腕を磨いた経験や、こだわり抜いた開発した独自メニューなど自慢の一品を提供している「キッチンカー」のNo.1を決定します！アジア最大級のサッカースタジアム「埼玉スタジアム2002」で、普段は味わえない味をお楽しみください。</p>
+							レストランで腕を磨いた経験や、こだわり抜いた開発した独自メニューなど自慢の一品を提供している「キッチンカー」のジャンル別のNo.1を決定します！アジア最大級のサッカースタジアム「埼玉スタジアム2002」で、普段は味わえない味をお楽しみください。</p>
 						</div>
 						<div class="frontEvent__map"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/map.png" alt="">
 								<p>埼玉高速鉄道「浦和美園駅」から徒歩15分</p><small>※駐車台数に限りがありますので、公共交通機関にてご来場ください。なお、満車の際はご容赦ください。</small>
