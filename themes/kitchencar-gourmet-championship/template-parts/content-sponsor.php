8a1c0014db0aac8afb786b93c8a3628cdef22aab
Susumu <seino@susu.mu>
2017-09-04 13:50:11 +0900

    KCC-73 協力のエリアを修正 #close

--- themes/kitchencar-gourmet-championship/template-parts/content-sponsor.php
+++ themes/kitchencar-gourmet-championship/template-parts/content-sponsor.php
@@ -6,15 +6,6 @@
 							<li class="frontSponsor__item">
 									<a target="_blank" href="http://foodear.jp/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/sponsor-foodear.png" alt=""></a>
 							</li>
-							<li class="frontSponsor__item">
-									<a target="_blank" href="http://www.reds-ss.com/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/sponsor-reds-support.png" width="60px" alt=""></a>
-							</li>
-							<li class="frontSponsor__item">
-									<a target="_blank" href="http://www.reds-ss.com/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/sponsor-reds.png" width="60px" alt=""></a>
-							</li>
-							<li class="frontSponsor__item">
-									<a href="https://www.w-tokyodo.com/contact" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/sponsor-zima.png" alt=""></a>
-							</li>
 					</ul>
 				</div>
 		</div>
