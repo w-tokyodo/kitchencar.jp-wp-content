24cb2cbc54a441b1e609cdf0f2c10935b8c57017
Susumu <seino@susu.mu>
2017-09-04 13:48:07 +0900

    KCC-67 ロゴイメージの修正
    ロゴの修正

--- themes/kitchencar-gourmet-championship/index.php
+++ themes/kitchencar-gourmet-championship/index.php
@@ -19,7 +19,7 @@ get_header(); ?>
 					<source src="<?php echo get_stylesheet_directory_uri(); ?>/movie/kitchencar_gorumet_grandprix_2017.m4v" type="video/mp4">
 				</video>
 				<div class="main-visual__logo">
-					<img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/main-visual.png" alt="">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/static/assets/img/main-visual-2017.png" alt="キッチンカーグルメ選手権2017">
 				</div>
 			</div>
 		</div>
