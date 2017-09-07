6e8353d0cd4354a65c090d94f42e4e039b6a2c2d
mimosafa <mimosafa@gmail.com>
2017-09-07 21:34:58 +0900

    f70a60e で追加した「お詫び」を削除
    埼スタOさん確認済み

--- themes/kitchencar-gourmet-championship/template-parts/content-information.php
+++ themes/kitchencar-gourmet-championship/template-parts/content-information.php
@@ -1,6 +1,5 @@
         <div class="frontNews" style="background-color:#333;color:#fff;text-align:center;">
             <div class="frontNews__inner">
-                <h3 style="font-weight:500;letter-spacing:.15em;">お詫び</h3>
                 <p>ランニングイベントの申込み方法については、ただ今、準備中でございます。<br>いましばらくお待ちください。&nbsp;<a href="2017/09/overview-and-run-2017/" style="color:#aaa;display:inline;">大会概要はこちらをご覧ください。</a></p>
             </div>
         </div>
