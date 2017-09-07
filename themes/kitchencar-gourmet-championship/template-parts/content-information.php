f70a60e41be98a91ee019209edabe0c30b60f470
mimosafa <mimosafa@gmail.com>
2017-09-07 18:19:48 +0900

    ↓ に見出し「お詫び」を追加

--- themes/kitchencar-gourmet-championship/template-parts/content-information.php
+++ themes/kitchencar-gourmet-championship/template-parts/content-information.php
@@ -1,5 +1,6 @@
         <div class="frontNews" style="background-color:#333;color:#fff;text-align:center;">
             <div class="frontNews__inner">
+                <h3 style="font-weight:500;letter-spacing:.15em;">お詫び</h3>
                 <p>ランニングイベントの申込み方法については、ただ今、準備中でございます。<br>いましばらくお待ちください。</p>
             </div>
         </div>
