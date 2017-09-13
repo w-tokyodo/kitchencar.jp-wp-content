7dba76c9d58b72051b0bc744ed9f4c936af3e9d1
mimosafa <mimosafa@gmail.com>
2017-09-13 09:21:15 +0900

    KCC-90 キッチンカー選手権エントリー／ランニングイベントお申込み <<< とりいそぎ

--- themes/kitchencar-gourmet-championship/template-parts/content-information.php
+++ themes/kitchencar-gourmet-championship/template-parts/content-information.php
@@ -1,5 +1,31 @@
         <div class="frontNews" style="background-color:#333;color:#fff;text-align:center;">
-            <div class="frontNews__inner">
-                <p>ランニングイベントの申込み方法については、ただ今、準備中でございます。<br>いましばらくお待ちください。&nbsp;<a href="2017/09/overview-and-run-2017/" style="color:#aaa;display:inline;">大会概要はこちらをご覧ください。</a></p>
+            <div class="frontNews__inner col-3" style="padding-bottom:30px;">
+                <div class="col-3__item" style="height:100px;text-align:left;font-size:.9em;">
+                    キッチンカー選手権出店エントリーとランニングイベント参加の応募エントリーはこちらからお願い致します。
+                </div>
+                <div class="col-3__item" style="height:100px;">
+                    <a href="http://kitchencar.jp/2017/09/entry2017/" style="
+                        display: block;
+                        width: 100%;
+                        height: 100%;
+                        text-align: center;
+                        font-weight: 700;
+                        background-color: #AF0B0C;
+                        color: #fff;
+                        line-height:80px;
+                    ">キッチンカー選手権出店応募</a>
+                </div>
+                <div class="col-3__item" style="height:100px;">
+                    <a href="https://www.sportsentry.ne.jp/event/t/72251" target="_blank" style="
+                        display: block;
+                        width: 100%;
+                        height: 100%;
+                        text-align: center;
+                        font-weight: 700;
+                        background-color: #0094d4;
+                        color: #fff;
+                        line-height:80px;
+                    ">ランニングイベントお申込み</a>
+                </div>
             </div>
         </div>
