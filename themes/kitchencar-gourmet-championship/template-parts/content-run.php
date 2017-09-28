a8b6925076da139cd8ef8802bd111f1ec0afd32a
Susumu <seino@susu.mu>
2017-09-28 17:50:20 +0900

    KCC-92 ランイベントの詳細ブロックの作成
    Runブロックパーツの作成

---
+++ themes/kitchencar-gourmet-championship/template-parts/content-run.php
@@ -0,0 +1,13 @@
+<div class="frontRun">
+	<div class="frontRun__inner">
+		<div class="frontRun__copy">
+			<img src="<?php echo get_stylesheet_directory_uri();?>/static/assets/img/run_copy.png" alt="サッカー日本代表がW杯出場を決めた「埼スタ」を走りつくす貴重なチャンス！">
+			<div class="frontRun__attention">
+				Runイベントの参加は有料です。グルメイベントのみの参加も可能です。(入場料無料)
+			</div>
+		</div>
+		<div class="frontRun__icon">
+			<img src="<?php echo get_stylesheet_directory_uri();?>/static/assets/img/run_desc.png" alt="Runイベントとスタジアムグルメが合体！">
+		</div>
+	</div>
+</div>
