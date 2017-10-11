0c57fbcee9a983731377e9430ac3b10d150649f7
mimosafa <mimosafa@gmail.com>
2017-10-11 20:54:49 +0900

    ローソンチケット

--- themes/kitchencar-gourmet-championship/template-parts/content-information.php
+++ themes/kitchencar-gourmet-championship/template-parts/content-information.php
@@ -1,19 +1,7 @@
         <div class="frontNews" style="background-color:#333;color:#fff;text-align:center;">
             <div class="frontNews__inner col-3" style="padding-bottom:30px;">
                 <div class="col-3__item" style="height:100px;text-align:left;font-size:.9em;">
-                    キッチンカー選手権出店エントリーとランニングイベント参加の応募エントリーはこちらからお願い致します。
-                </div>
-                <div class="col-3__item" style="height:100px;">
-                    <a href="http://kitchencar.jp/2017/09/entry2017/" style="
-                        display: block;
-                        width: 100%;
-                        height: 100%;
-                        text-align: center;
-                        font-weight: 700;
-                        background-color: #AF0B0C;
-                        color: #fff;
-                        line-height:80px;
-                    ">キッチンカー選手権出店応募</a>
+                    申し込み多数につき、ランニングイベント参加募集を10/20（金）まで延長いたします。たくさんのお申し込みをお待ちいたしております。
                 </div>
                 <div class="col-3__item" style="height:100px;">
                     <a href="https://www.sportsentry.ne.jp/event/t/72251" target="_blank" style="
@@ -25,7 +13,25 @@
                         background-color: #0094d4;
                         color: #fff;
                         line-height:80px;
-                    ">ランニングイベントお申込み</a>
+                        margin: 0;
+                        padding: 0;
+                        border-width: 0;
+                    " class="btn">ランニングイベントお申込み</a>
+                </div>
+                <div class="col-3__item" style="height:100px;">
+                    <a href="https://do.l-tike.com/app/dss/race/detail?acd=1l3RSD-yDRx" target="_blank" style="
+                        display: block;
+                        width: 100%;
+                        height: 100%;
+                        text-align: center;
+                        font-weight: 700;
+                        background-color: #076cb9;
+                        color: #fff;
+                        line-height:80px;
+                        margin: 0;
+                        padding: 0;
+                        border-width: 0;
+                    " class="btn">ローソンチケット会員の方</a>
                 </div>
             </div>
         </div>
