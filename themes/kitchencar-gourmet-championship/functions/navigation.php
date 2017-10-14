3cd055fdd477d2e7bf7f0b3a5645b81237d06cd4
Susumu <seino@susu.mu>
2017-10-14 17:04:13 +0900

    KCC-94 グローバルナビゲーションの設置
    登録したナビゲーションの削除

--- themes/kitchencar-gourmet-championship/functions/navigation.php
+++ themes/kitchencar-gourmet-championship/functions/navigation.php
@@ -1,19 +1,1 @@
 <?php
-$kccar_navi = array(
-	'menu'            => '',
-	'menu_class'      => 'menu',
-	'menu_id'         => '{メニューのスラッグ}-{連番}',
-	'container'       => 'div',
-	'container_class' => 'menu-{メニューのスラッグ}-container',
-	'container_id'    => '',
-	'fallback_cb'     => 'wp_page_menu',
-	'before'          => '',
-	'after'           => '',
-	'link_before'     => '',
-	'link_after'      => '',
-	'echo'            => true,
-	'depth'           => 0,
-	'walker'          => '',
-	'theme_location'  => '',
-	'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-);
