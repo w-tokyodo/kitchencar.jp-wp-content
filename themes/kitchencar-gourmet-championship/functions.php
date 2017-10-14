78c101d76c0f190f1b57b70b278fd3c4e26d4d8d
Susumu <seino@susu.mu>
2017-10-14 16:12:58 +0900

    KCC-94 グローバルナビゲーションの設置
    ナビゲーションの追加

--- themes/kitchencar-gourmet-championship/functions.php
+++ themes/kitchencar-gourmet-championship/functions.php
@@ -108,6 +108,7 @@ function kitchincar_gourmet_championship_scripts() {
 	wp_enqueue_script( 'kitchincar-gourmet-championship-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'kitchincar-gourmet-championship-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -199,3 +200,5 @@ add_image_size( 'kgc_thumbnail_post', 400, 400, true );
 add_image_size( 'kgc_thumbnail_car', 100, 100, true );
 add_image_size( 'kgc-post-image', 400, 150, true );
 add_image_size( 'kgc_news_thumbnail', 200, 200, array( 'center', 'center' ) );
+
+require_once locate_template('functions/navigation.php');
