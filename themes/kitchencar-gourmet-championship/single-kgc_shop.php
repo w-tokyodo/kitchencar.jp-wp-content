387167d185290e64de89ddaca9a9c6877dbf2f73
mimosafa <mimosafa@gmail.com>
2017-10-25 20:37:33 +0900

    kgc_shop 個別ページをアップデート
    エリアの情報、ほかのエリアへのリンクを追加

--- themes/kitchencar-gourmet-championship/single-kgc_shop.php
+++ themes/kitchencar-gourmet-championship/single-kgc_shop.php
@@ -68,7 +68,49 @@ get_header(); ?>
 						</dl>
 					</div>
 				</div>
+			</div><?php
+
+			/**
+			 * エリア情報
+			 */
+			if ( $areas = get_the_terms( $post->ID, 'kgc_area' ) ) :
+				$area = $areas[0];
+				$map = SCF::get_term_meta( $area, 'kgc_area', 'kgc2017_area_map' );
+				$area_color = get_term_meta( $area->term_id, 'color', true ) ?: 'inherit'; ?>
+
+			<div class="archive-area__headline" style="text-align:center">
+
+				<p style="font-family:serif;font-size:1.7em;">出店エリア</p>
+				<div><?= wp_get_attachment_image( $map, 'full' ) ?></div>
+
+				<p style="font-family:serif;"><?= esc_html( $area->name ) ?>の他の出店者を見る</p>
+				<ul class="cat-list">
+					<li class="cat-item" style="line-height:2em;">
+						<a href="<?= esc_url( get_term_link( $area, 'kgc_area' ) ) ?>" style="background-color:<?= esc_attr( $area_color ) ?>"><?= esc_html( $area->name ) ?></a>
+					</li>
+				</ul>
+
+				<p style="font-family:serif;">
+					その他のエリアも見る
+					<small><a href="<?= esc_url( get_post_type_archive_link( 'kgc_shop' ) ) ?>">&gt;&gt; すべてのキッチンカー</a></small>
+				</p>
+				<ul class="cat-list"><?php
+					$other_areas = get_terms( array(
+						'taxonomy' => 'kgc_area',
+						'exclude' => $area->term_id
+					) );
+					foreach ( $other_areas as $other_area ) {
+						$other_area_color = get_term_meta( $other_area->term_id, 'color', true ) ?: 'inherit';
+						echo '<li class="cat-item" style="line-height:2em;margin-right:5px;">';
+						echo '<a href="' . esc_url( get_term_link( $other_area, 'kgc_area' ) ) . '" style="background-color:' . esc_attr( $other_area_color ) . '">' . esc_html( $other_area->name ) . '</a>';
+						echo '</li>';
+					} ?>
+				</ul>
 			</div>
+
+<?php
+			endif; ?>
+
 		</article>
 	<?php endwhile; ?>
 
